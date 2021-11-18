42fb6ab3805485649f002f62d1514d8261e18b35
ALexStone19 <alexstone19654@gmail.com>
2021-11-18 11:42:05 +0200

    Added routes, components structure

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,53 @@
+<template>
+   <section>FIlter</section>
+   <section>
+      List of coaches
+      <div class="controls">
+         <button>Refresh</button>
+         <router-link to="/register">Register as coach</router-link>
+         <ul v-if="hasCoaches">
+            <coaches-item
+               v-for="coach in filteredCoaches"
+               :key="coach.id"
+               :id="coach.id"
+               :first-name="coach.firstName"
+               :last-name="coach.lastName"
+               :rate="coach.hourlyRate"
+               :areas="coach.areas"
+            ></coaches-item>
+         </ul>
+         <h3 v-else>No coaches</h3>
+      </div>
+   </section>
+</template>
+
+<script>
+import CoachesItem from "../coaches/CoachItem.vue";
+
+export default {
+   components: {
+      CoachesItem
+   },
+   computed: {
+      filteredCoaches() {
+         return this.$store.getters["coaches/coaches"];
+      },
+      hasCoaches() {
+         return this.$store.getters["coaches/hasCoaches"];
+      }
+   }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegister from "./pages/coaches/CoachRegister.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestRecieved from "./pages/requests/RequestsRecieved.vue";
+import NotFound from "./pages/NotFound.vue";
+
+
+const router = createRouter({
+   history: createWebHistory(),
+   routes: [
+      {
+         path: "/",
+         redirect: "/coaches"
+      },
+      {
+         path: "/coaches",
+         component: CoachesList
+      }, 
+      {
+         path: "/coaches/:id",
+         component: CoachDetail,
+         children: [{
+            path: "contact",
+            component: ContactCoach
+         }]
+      },
+      {
+         path: "/register",
+         component: CoachRegister
+      },
+      {
+         path: "/requests",
+         component: RequestRecieved
+      },
+      {
+         path: "/:notFound(.*)",
+         component: NotFound
+      }
+   ]
+});
+
+export default router;
