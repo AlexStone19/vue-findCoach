8eb0bb862750c0e69fdd51163561bf283da74205
ALexStone19 <alexstone19654@gmail.com>
2021-11-18 12:59:20 +0200

    Added base components, built Coach list, item and details components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,23 +1,25 @@
 <template>
    <section>FIlter</section>
    <section>
-      List of coaches
-      <div class="controls">
-         <button>Refresh</button>
-         <router-link to="/register">Register as coach</router-link>
+      <base-card>
+         List of coaches
+         <div class="controls">
+            <base-button mode="outline">Refresh</base-button>
+            <base-button link to="/register">Register as coach</base-button>
+         </div>
          <ul v-if="hasCoaches">
-            <coaches-item
-               v-for="coach in filteredCoaches"
-               :key="coach.id"
-               :id="coach.id"
-               :first-name="coach.firstName"
-               :last-name="coach.lastName"
-               :rate="coach.hourlyRate"
-               :areas="coach.areas"
-            ></coaches-item>
+               <coaches-item
+                  v-for="coach in filteredCoaches"
+                  :key="coach.id"
+                  :id="coach.id"
+                  :first-name="coach.firstName"
+                  :last-name="coach.lastName"
+                  :rate="coach.hourlyRate"
+                  :areas="coach.areas"
+               ></coaches-item>
          </ul>
          <h3 v-else>No coaches</h3>
-      </div>
+      </base-card>
    </section>
 </template>
 
